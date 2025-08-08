8cafc97ec626106becfb2efd3eb1248ec5bd7c55
yizhuoliang <yizhuo.liang.hello@gmail.com>
2025-08-07 19:16:23 -0700

    typo

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from 'next'
 
 const SITE_URL = 'https://yizhuoliang.github.io'
 
+export const dynamic = 'force-static'
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
